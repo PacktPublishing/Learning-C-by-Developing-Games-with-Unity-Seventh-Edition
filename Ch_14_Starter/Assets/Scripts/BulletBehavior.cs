a75e4f65784bbd2feeb65aa70a79236815fedf2e
Harrison Ferrone <hferrone@gmail.com>
2023-08-01 12:48:24 +0200

    Remove old files

--- Ch_14_Starter/Assets/Scripts/BulletBehavior.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BulletBehavior : Destroyable<BulletBehavior>
-{
-    // All bullets are now destroyable using the generic Destroyable class
-}
